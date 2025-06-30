4d771fa9bbdcd54b9ab6b0dc2b28c1ecb00597f5
MohammedMostafa2000 <mohamedmos574@gmail.com>
2025-06-30 17:05:34 +0300

    importing the screenUtils package

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:movies_app/config/theme_manager.dart';
 import 'package:movies_app/core/routes_manager.dart';
 
@@ -11,14 +12,24 @@ class MoviesApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      onGenerateRoute: RoutesManager.goRoute,
-      initialRoute: RoutesManager.mainLayoutView,
-      locale: Locale('en'),
-      theme: ThemeManager.light,
-      darkTheme: ThemeManager.dark,
-      themeMode: ThemeMode.dark,
+    return ScreenUtilInit(
+      designSize: Size(context.width, context.height),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context, child) => MaterialApp(
+        debugShowCheckedModeBanner: false,
+        onGenerateRoute: RoutesManager.goRoute,
+        initialRoute: RoutesManager.mainLayoutView,
+        locale: Locale('en'),
+        theme: ThemeManager.light,
+        darkTheme: ThemeManager.dark,
+        themeMode: ThemeMode.dark,
+      ),
     );
   }
+}
+
+extension ContextEx on BuildContext {
+  double get width => MediaQuery.sizeOf(this).width;
+  double get height => MediaQuery.sizeOf(this).height;
 }
