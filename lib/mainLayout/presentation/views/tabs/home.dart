acb2035f85c4e01c0d9f19bc06faa7d3e0a7f140
MohammedMostafa2000 <mohamedmos574@gmail.com>
2025-07-03 22:59:21 +0300

    finish el logic bta3 home tab

--- lib/mainLayout/presentation/views/tabs/home.dart
+++ lib/mainLayout/presentation/views/tabs/home.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -14,155 +15,181 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  String movieCategory = 'Action';
+  List<String> movieCategories = [
+    'Action',
+    'Adventure',
+    'Animation',
+    'Anime',
+    'Biography',
+    'Comedy',
+    'Crime',
+    'Documentary',
+    'Drama',
+    'Family',
+    'Fantasy',
+    'Horror',
+    'Music',
+    'Musical',
+    'Mystery',
+    'Romance',
+    'Sci-Fi',
+    'Sport',
+    'Thriller',
+    'Western',
+  ];
+  int _selectedIndex = 0;
   late HomeTabViewModel homeTabViewModel;
-
   bool isMoreTabbed = false;
   String toggleLabel = 'See More  ->';
 
   @override
   void initState() {
     super.initState();
-    homeTabViewModel = HomeTabViewModel();
+    homeTabViewModel = Provider.of<HomeTabViewModel>(context, listen: false);
     homeTabViewModel.getAvailableMovies();
-    homeTabViewModel.getActionMovies();
+    homeTabViewModel.getMoviesBasedOnGenre(genre: movieCategories[homeTabViewModel.categoryIndex]);
+    movieCategory = movieCategories[homeTabViewModel.categoryIndex];
   }
 
   @override
   void dispose() {
     super.dispose();
-    homeTabViewModel.dispose();
+    homeTabViewModel.incrementCategoryIndex();
+    homeTabViewModel.genreMoviesList.clear();
   }
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider.value(
-      value: homeTabViewModel,
-      child: Stack(
-        children: [
-          Container(
-            height: 645.h,
-            width: double.infinity,
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                opacity: 0.3,
-                fit: BoxFit.fill,
-                image: AssetImage('assets/images/movie.jpg'),
+    return Consumer<HomeTabViewModel>(
+        builder: (context, viewModel, child) => Stack(children: [
+              Container(
+                height: 645.h,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                      opacity: 0.3,
+                      fit: BoxFit.fill,
+                      image: CachedNetworkImageProvider(homeTabViewModel
+                              .availableMoviesList.isNotEmpty
+                          ? homeTabViewModel.availableMoviesList[_selectedIndex].largeCoverImage
+                          : 'https://n-lightenment.com/wp-content/uploads/2015/10/movie-night11.jpg')),
+                ),
               ),
-            ),
-          ),
-          SafeArea(
-            child: Consumer<HomeTabViewModel>(builder: (context, viewModel, child) {
-              return CustomScrollView(
-                physics: BouncingScrollPhysics(),
-                slivers: [
-                  SliverToBoxAdapter(
-                    child: Image.asset(
-                      AssetsManager.availableNow,
-                      height: 94.h,
-                      width: 266.w,
-                    ),
-                  ),
-                  SliverToBoxAdapter(
-                    child: SizedBox(
-                      height: 350.h,
-                      width: double.infinity,
-                      child: CarouselSlider(
-                          options: CarouselOptions(
-                            height: double.infinity,
-                            enlargeCenterPage: true,
-                            viewportFraction: 0.62,
-                            aspectRatio: 1,
-                          ),
-                          items: viewModel.availableMoviesList
-                              .map(
-                                (movieDataModel) => MovieCard(
-                                  movieDataModel: movieDataModel,
-                                ),
-                              )
-                              .toList()),
-                    ),
-                  ),
-                  SliverToBoxAdapter(
-                    child: Image.asset(
-                      AssetsManager.watchNow,
-                      height: 146.h,
-                      width: 330.w,
-                    ),
-                  ),
-                  SliverToBoxAdapter(child: SizedBox(height: 24.h)),
-                  SliverToBoxAdapter(
-                    child: Padding(
-                      padding: REdgeInsets.symmetric(horizontal: 8.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            'Action',
-                            style: Theme.of(context).textTheme.labelMedium,
-                          ),
-                          InkWell(
-                            onTap: () {
-                              setState(() {
-                                isMoreTabbed = !isMoreTabbed;
-                                toggleLabel = isMoreTabbed ? 'See Less' : 'See More  ->';
-                              });
-                            },
-                            child: Text(
-                              toggleLabel,
-                              style: Theme.of(context).textTheme.labelSmall,
-                            ),
-                          ),
-                        ],
+              SafeArea(
+                child: CustomScrollView(
+                  physics: BouncingScrollPhysics(),
+                  slivers: [
+                    SliverToBoxAdapter(
+                      child: Image.asset(
+                        AssetsManager.availableNow,
+                        height: 94.h,
+                        width: 266.w,
                       ),
                     ),
-                  ),
-                  SliverToBoxAdapter(child: SizedBox(height: 12.h)),
-                  isMoreTabbed
-                      ? SliverToBoxAdapter(
-                          child: GridView.builder(
-                            padding: REdgeInsets.symmetric(horizontal: 8),
-                            physics: NeverScrollableScrollPhysics(),
-                            shrinkWrap: true,
-                            itemCount: viewModel.genreMoviesList.length,
-                            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                              crossAxisCount: 3,
-                              mainAxisSpacing: 8.h,
-                              crossAxisSpacing: 8.w,
-                              childAspectRatio: 0.6,
+                    SliverToBoxAdapter(
+                      child: SizedBox(
+                        height: 350.h,
+                        width: double.infinity,
+                        child: CarouselSlider(
+                            options: CarouselOptions(
+                              onPageChanged: (index, reason) {
+                                setState(() {
+                                  _selectedIndex = index;
+                                });
+                              },
+                              height: double.infinity,
+                              enlargeCenterPage: true,
+                              viewportFraction: 0.62,
+                              aspectRatio: 1,
                             ),
-                            itemBuilder: (context, index) => MovieCard(
-                              movieDataModel: viewModel.genreMoviesList[index],
+                            items: viewModel.availableMoviesList
+                                .map(
+                                  (movieDataModel) => MovieCard(
+                                    movieDataModel: movieDataModel,
+                                  ),
+                                )
+                                .toList()),
+                      ),
+                    ),
+                    SliverToBoxAdapter(
+                      child: Image.asset(
+                        AssetsManager.watchNow,
+                        height: 146.h,
+                        width: 330.w,
+                      ),
+                    ),
+                    SliverToBoxAdapter(child: SizedBox(height: 24.h)),
+                    SliverToBoxAdapter(
+                      child: Padding(
+                        padding: REdgeInsets.symmetric(horizontal: 8.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              movieCategory,
+                              style: Theme.of(context).textTheme.labelMedium,
                             ),
-                          ),
-                        )
-                      : SliverToBoxAdapter(
-                          child: SizedBox(
-                            height: 220,
-                            child: ListView.separated(
-                              physics: BouncingScrollPhysics(),
+                            InkWell(
+                              onTap: () {
+                                setState(() {
+                                  isMoreTabbed = !isMoreTabbed;
+                                  toggleLabel = isMoreTabbed ? 'See Less' : 'See More  ->';
+                                });
+                              },
+                              child: Text(
+                                toggleLabel,
+                                style: Theme.of(context).textTheme.labelSmall,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    SliverToBoxAdapter(child: SizedBox(height: 12.h)),
+                    isMoreTabbed
+                        ? SliverToBoxAdapter(
+                            child: GridView.builder(
                               padding: REdgeInsets.symmetric(horizontal: 8),
-                              scrollDirection: Axis.horizontal,
-                              separatorBuilder: (context, index) => SizedBox(width: 16.w),
-                              itemCount: viewModel.genreMoviesList.length > 10
-                                  ? 10
-                                  : viewModel.genreMoviesList.length,
-                              itemBuilder: (context, index) => SizedBox(
-                                height: 220.h,
-                                width: 146.w,
-                                child: MovieCard(
-                                  movieDataModel: viewModel.genreMoviesList[index],
+                              physics: NeverScrollableScrollPhysics(),
+                              shrinkWrap: true,
+                              itemCount: viewModel.genreMoviesList.length,
+                              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                                crossAxisCount: 3,
+                                mainAxisSpacing: 8.h,
+                                crossAxisSpacing: 8.w,
+                                childAspectRatio: 0.6,
+                              ),
+                              itemBuilder: (context, index) => MovieCard(
+                                movieDataModel: viewModel.genreMoviesList[index],
+                              ),
+                            ),
+                          )
+                        : SliverToBoxAdapter(
+                            child: SizedBox(
+                              height: 220,
+                              child: ListView.separated(
+                                physics: BouncingScrollPhysics(),
+                                padding: REdgeInsets.symmetric(horizontal: 8),
+                                scrollDirection: Axis.horizontal,
+                                separatorBuilder: (context, index) => SizedBox(width: 16.w),
+                                itemCount: viewModel.genreMoviesList.length > 10
+                                    ? 10
+                                    : viewModel.genreMoviesList.length,
+                                itemBuilder: (context, index) => SizedBox(
+                                  height: 220.h,
+                                  width: 146.w,
+                                  child: MovieCard(
+                                    movieDataModel: viewModel.genreMoviesList[index],
+                                  ),
                                 ),
                               ),
                             ),
                           ),
-                        ),
-                  SliverToBoxAdapter(child: SizedBox(height: 20.h)),
-                ],
-              );
-            }),
-          ),
-        ],
-      ),
-    );
+                    SliverToBoxAdapter(child: SizedBox(height: 20.h)),
+                  ],
+                ),
+              ),
+            ]));
   }
 }
